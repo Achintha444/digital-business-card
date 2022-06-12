f9463d58d9b2ee40c2fd716a7d86bb26746e40d2
Achinths Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2022-06-12 20:28:54 +0530

    skeleton of the digital card is completed

---
+++ src/AboutInterestsComponent/AboutInterestsComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SkeletonComponet.css';
+
+export default function SkeletonComponet() {
+    return (
+        <div className='image'>
+
+        </div>
+    )
+}
---
+++ src/imageComponent/ImageComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ImageComponent.css';
+
+export default function ImageComponent() {
+    return (
+        <div className='image'>
+
+        </div>
+    )
+}
